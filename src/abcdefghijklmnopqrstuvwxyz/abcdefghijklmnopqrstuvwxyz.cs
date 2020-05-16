cd7141a06e5c3e2151bd1d7fe291152f5d8c95b6
Sajid <sajidur78@gmail.com>
2020-05-16 16:16:42 +0600

    Initial Commit

---
+++ src/abcdefghijklmnopqrstuvwxyz/abcdefghijklmnopqrstuvwxyz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace abcdefghijklmnopqrstuvwxyz
+{
+    public class AlphabetProvider
+    {
+        public static string GetAlphabet()
+            => "abcdefghijklmnopqrstuvwxyz";
+    }
+}
